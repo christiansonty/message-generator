b998c0d844077502b5eb370ddf10336acdcd69fe
Ty Christianson <tychristianson@hotmail.com>
2023-07-19 16:06:45 -0400

    Created message arrays

---
+++ main.js
@@ -0,0 +1,3 @@
+const startMessage = ['You ', 'Your Spouse ', 'Your Pets ', 'Your Kids ']; 
+const midMessage = ['need to beware of ', 'should grab ', 'want to wash ', 'are going to drive through ']; 
+const endMessage = ['many banannas.', 'a room full of chinchillas.', 'a hungry lion.'];
