c5e75c0f90e950b7d7ebe3aba12b1dc9d04119f7
Ty Christianson <tychristianson@hotmail.com>
2023-07-19 16:10:30 -0400

    Add function for random selection

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const startMessage = ['You ', 'Your Spouse ', 'Your Pets ', 'Your Kids ']; 
 const midMessage = ['need to beware of ', 'should grab ', 'want to wash ', 'are going to drive through ']; 
 const endMessage = ['many banannas.', 'a room full of chinchillas.', 'a hungry lion.'];
+
+
+const randMessage = msgArray => {
+    const len = msgArray.length; 
+    const randPos = Math.floor(Math.random() * len)
+    return msgArray[randPos];
+}
